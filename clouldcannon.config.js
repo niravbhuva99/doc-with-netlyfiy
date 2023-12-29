3d83383c48a96ceca2d3b38ef06faf33ddbf3cc4
Nirav Bhuva <niravbhuva99@gmail.com>
2023-12-29 16:27:59 +0000

    Updated 1 file via CloudCannon.

--- clouldcannon.config.js
+++ clouldcannon.config.js
@@ -2,7 +2,7 @@ module.exports = {
  
     collections_config: {
     blog: {
-      path: "/blog",
+      path: "docs",
       output: true,
       url: "/blog/[slug].html",
       icon: "event_available"
